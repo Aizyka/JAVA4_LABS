70e2fab98e114360dc5d80fef4e97b8e45e2d85a
Aizyka <seng_1111@mail.ru>
2024-02-19 03:35:27 +0300

    Update Mapping.java

--- src/main/java/home/masterserver/Mapping.java
+++ src/main/java/home/masterserver/Mapping.java
@@ -3,8 +3,6 @@ package home.masterserver;
 import org.json.JSONObject;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
