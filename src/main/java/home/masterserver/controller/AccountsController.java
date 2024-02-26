4439066c902f13aa379f0dac08eb7103a5ff0a11
Aizyka <seng_1111@mail.ru>
2024-02-26 11:22:38 +0300

    Update AccountsController.java

--- src/main/java/home/masterserver/controller/AccountsController.java
+++ src/main/java/home/masterserver/controller/AccountsController.java
@@ -3,7 +3,6 @@ package home.masterserver.controller;
 import org.json.JSONObject;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
